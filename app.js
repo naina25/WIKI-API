4be5fa98d920988f660e515a750188096b07c3c5
naina <nanu02d8@gmail.com>
2020-12-22 17:21:23 +0530

    Connect database and create article collection

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
+mongoose.connect('mongodb://localhost/wikiDB', {useNewUrlParser: true, useUnifiedTopology: true});
+const articleSchema = {
+    title: String,
+    content: String
+}
+const article  = mongoose.model("Article", articleSchema)
+
 app.use(bodyParser.urlencoded({
   extended: true
 }));
