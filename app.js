c535f878a40b06598f5fbfd93ca670f25d3c09b4
naina <nanu02d8@gmail.com>
2020-12-22 17:58:54 +0530

    Add get route for all articles

--- app.js
+++ app.js
@@ -7,19 +7,27 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-mongoose.connect('mongodb://localhost/wikiDB', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost:27017/wikiDb', {useNewUrlParser: true, useUnifiedTopology: true});
 const articleSchema = {
     title: String,
     content: String
 }
-const article  = mongoose.model("Article", articleSchema)
+const Article  = mongoose.model("Article", articleSchema)
 
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(express.static("public"));
 
-//TODO
+app.get("/articles", (req, res) => {
+    Article.find((err, foundArticles) => {
+        if(!err){
+            res.send(foundArticles);
+        }else{
+            res.send(err);
+        }
+    })  
+})
 
 app.listen(3030, function() {
   console.log("Server started on port 3030");
